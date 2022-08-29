87932643ef43f8d72dcca1b10d6150e559e3a8ce
Mariusz <maniek.software@gmail.com>
2022-08-29 17:55:28 +0200

    refaktor i dodanie nowej funkcjonalności

--- todo.js
+++ todo.js
@@ -15,10 +15,10 @@ function addElement() {
         msg.textContent = 'Nie podano wartości. Element nie został dodany do listy!';
     } else {
         let item = document.createElement('li');
-        item.textContent = textInput.value
+        item.textContent = textInput.value;
         list.appendChild(item);
         textInput.value = '';
-        msg.textContent = 'Kolejne elementy będą ukazane na liście poniżej. Aby usunąć wybrany element, należy na nim kliknąć.';
+        msg.textContent = 'Kolejne elementy będą ukazane na liście poniżej. Aby usunąć wybrany element, należy na nim kliknąć i potwierdzić operację.';
     }
 }
 
@@ -32,12 +32,43 @@ function removeLastElement() {
     }
 }
 
-// Dodać modal, który otwiera się po kliknięciu w element listy i to na nim potwierdzamy usunięcie elementu.
-
 function removeElement(e) {
     this.removeChild(e.target);
+    console.log('Element został usunięty!');
 }
 
 addBtn.addEventListener('click', addElement);
 delBtn.addEventListener('click', removeLastElement);
-list.addEventListener('click', removeElement);
+list.addEventListener('click', openModal);
+
+// Dodać modal, który otwiera się po kliknięciu w element listy i to na nim potwierdzamy usunięcie elementu.
+
+// Trzeba po DOM cofnąć się do body, potem zejśc do section -> list i odnaleźć element do usunięcia
+
+const modal = document.getElementById('myModal');
+const modalBtn = document.getElementById('modalBtn');
+const modalCloseBtn = document.getElementsByClassName('close')[0];
+const confirmBtn = document.getElementById('confirm');
+const rejectBtn = document.getElementById('reject');
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function (e) {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+}
+
+modalBtn.addEventListener('click', openModal);
+modalCloseBtn.addEventListener('click', closeModal);
+
+confirmBtn.addEventListener('click', closeModal);
+confirmBtn.addEventListener('click', removeElement);
+
+rejectBtn.addEventListener('click', closeModal);
