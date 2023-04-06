ec9bd70e7eea6da63e20db85939f4c7aed345051
Mariusz <maniek.software@gmail.com>
2023-04-06 16:08:47 +0200

    refactor and redesign

--- todo.js
+++ todo.js
@@ -1,56 +1,50 @@
-// Dodawanie i usuwanie elementów
+// Add and remove elements
+const textInput = document.getElementById("text-input");
+const addBtn = document.getElementById("add-button");
+const delBtn = document.getElementById("remove-button");
 
-const textInput = document.getElementById('text-input');
-const addBtn = document.getElementById('add-button');
-const delBtn = document.getElementById('remove-button');
+// Message
+const msg = document.getElementById("msg");
 
-// Komunikat
-
-const msg = document.getElementById('msg');
-
-// Lista elementów
-
-const section = document.querySelector('section');
-const list = document.createElement('ol');
+// Elements list
+const section = document.getElementById("preview");
+const list = document.createElement("ol");
 section.appendChild(list);
 
 // Modal
+const modal = document.getElementById("myModal");
+const modalCloseBtn = document.getElementsByClassName("close")[0];
+const confirmBtn = document.getElementById("confirm");
+const rejectBtn = document.getElementById("reject");
 
-const modal = document.getElementById('myModal');
-const modalCloseBtn = document.getElementsByClassName('close')[0];
-const confirmBtn = document.getElementById('confirm');
-const rejectBtn = document.getElementById('reject');
-
-// Kliknięty element
-
+// Clicked element variable
 let clickedElement;
 
-// Funkcje dodawania i usuwania elementu
-
+// Add and remove element functions
 function addElement() {
-    if (textInput.value === '') {
-        msg.textContent = 'Nie podano wartości. Element nie został dodany do listy!';
+    if (textInput.value === "") {
+        msg.textContent =
+            "Nie podano wartości. Element nie został dodany do listy!";
     } else {
-        let item = document.createElement('li');
+        let item = document.createElement("li");
         item.textContent = textInput.value;
         list.appendChild(item);
-        textInput.value = '';
-        msg.textContent = 'Kolejne elementy będą ukazane na liście poniżej. Aby usunąć wybrany element, należy na nim kliknąć i potwierdzić operację.';
+        textInput.value = "";
+        msg.textContent =
+            "Kolejne elementy będą ukazane na liście poniżej. Aby usunąć wybrany element, należy na nim kliknąć i potwierdzić operację.";
     }
 }
 
 function removeLastElement() {
     let lastItem = list.lastChild;
     if (lastItem === null) {
-        msg.textContent = 'Lista jest pusta. Dodaj elementy, by móc je usuwać.';
+        msg.textContent = "Lista jest pusta. Dodaj elementy, by móc je usuwać.";
     } else {
-        //lastItem.parentNode.removeChild(lastItem);
         lastItem.remove();
     }
 }
 
-// Funkcje przechowywania i usuwania wybranego elementu
-
+// Store and remove clicked element functions
 function storeClickedElement(e) {
     clickedElement = e.target;
     console.log(clickedElement.textContent);
@@ -58,42 +52,39 @@ function storeClickedElement(e) {
 
 function removeElement() {
     clickedElement.remove();
-    msg.textContent = 'Wybrany element został usunięty!';
+    clickedElement = undefined;
+    msg.textContent = "Wybrany element został usunięty!";
 }
 
-// Funkcje modala
-
+// Modal functions
 function openModal() {
-    modal.style.display = 'block';
+    modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = 'none';
+    modal.style.display = "none";
 }
 
 window.onclick = function (e) {
     if (e.target === modal) {
-        modal.style.display = 'none';
+        modal.style.display = "none";
     }
-}
+};
 
-// Eventy dla przycisków dodawania i usuwania elementu
+// Add and remove buttons events
+addBtn.addEventListener("click", addElement);
+delBtn.addEventListener("click", removeLastElement);
 
-addBtn.addEventListener('click', addElement);
-delBtn.addEventListener('click', removeLastElement);
-
-// Eventy dla modala
-
-modalCloseBtn.addEventListener('click', closeModal);
-rejectBtn.addEventListener('click', closeModal);
-confirmBtn.addEventListener('click', () => {
+// Modal Events
+modalCloseBtn.addEventListener("click", closeModal);
+rejectBtn.addEventListener("click", closeModal);
+confirmBtn.addEventListener("click", () => {
     closeModal();
     removeElement();
 });
 
-// Eventy dla listy
-
-list.addEventListener('click', (e) => {
+// Items list elements
+list.addEventListener("click", (e) => {
     openModal();
     storeClickedElement(e);
 });
